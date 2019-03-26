669ebfe5030a0f6baa8bd5d7b0a3e9b2896d7244
DamienDOUMER <damientohin@gmail.com>
2019-03-26 17:55:34 +0100

    Completed functional client

--- NotificationClient/Program.cs
+++ NotificationClient/Program.cs
@@ -1,12 +1,61 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace NotificationClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string protocole = "https";
+            string host = "localhost:44357";
+
+            var connection = new HubConnectionBuilder()
+                .WithUrl($"{protocole}://{host}/api/notification").Build();
+
+            connection.Closed += ex =>
+            {
+                Console.WriteLine($"Connection Closed: {ex?.Message}");
+                return Task.CompletedTask;
+            };
+
+            connection.On<ContactNotificationDTO>("ContactModified", notif =>
+            {
+                Console.WriteLine($"Received notification {notif}");
+            });
+
+            Console.WriteLine("Connecting...");
+            await connection.StartAsync();
+            Console.WriteLine("Connection waiting");
+
+            Console.ReadLine();
         }
+    }
+
+
+    public class NotificationDTO
+    {
+        public int Id { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public int RecipientId { get; set; }
+        /// <summary>
+        /// The person who caused the notification to occure
+        /// </summary>
+        public int SenderId { get; set; }
+    }
+
+    public enum ContactNotificationType
+    {
+        ContactModified,
+        ContactDeleted
+    }
+
+    public class ContactNotificationDTO : NotificationDTO
+    {
+        public ContactNotificationType Type { get; set; }
+        public int ContactId { get; set; }
+        public IDictionary<string, string> Properties { get; set; } = new Dictionary<string, string>();
     }
 }
